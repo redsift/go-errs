bcf6c23cbcd808168314e7b25d4c12e0fa24d889
Julian Picht <julian.picht@redsift.io>
2026-09-09 17:45:21 +0200

    fix missing cause in Errorf

--- fmt.go
+++ fmt.go
@@ -18,20 +18,27 @@ func Errorf(format string, args ...any) error {
 			continue
 		}
 
-		args[i] = errors.New(perr.Detail)
+		if perr.cause != nil {
+			args[i] = perr.cause
+		} else {
+			args[i] = errors.New(perr.Detail)
+		}
 	}
 
 	if perr == nil {
 		return fmt.Errorf(format, args...)
 	}
 
+	cause := fmt.Errorf(format, args...)
+
 	return &PropagatedError{
 		Id:     perr.Id,
 		Code:   perr.Code,
 		Title:  perr.Title,
-		Detail: fmt.Errorf(format, args...).Error(),
+		Detail: cause.Error(),
 		Link:   perr.Link,
 		Source: perr.Source,
 		Status: perr.Status,
+		cause:  cause,
 	}
 }
